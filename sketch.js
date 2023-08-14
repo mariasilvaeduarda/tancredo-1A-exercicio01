f9eae10cc6c0b8607ab2c559c6ef65f01cd5fbc3
Maria Eduarda da Silva <106679343+mariasilvaeduarda@users.noreply.github.com>
2023-08-14 11:51:10 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400, 400);
+background("rgb(243,53,86)");
+
+}
+function draw() {
+  
+  stroke("white")
+  fill("black")
+  rect(mouseX,mouseY,40,40)
+ 
+if(mouseIsPressed){}
+
+}
